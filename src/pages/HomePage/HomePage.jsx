94ad0152d5465406048c19575268eef4ea464814
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-08-22 06:43:00 +0300

    add homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import s from "./HomePage.module.css";
+import Picture from "../../assets/image/Picture.jpg";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <section className={s.hero}>
+      <div className={s.heroContainer}>
+        <h1 className={s.heroTitle}>Campers of your dreams</h1>
+        <p className={s.heroText}>
+          You can find everything you want in our catalog
+        </p>
+        <button className={s.heroBtn}>View Now</button>
+        {/* <img src={Picture} /> */}
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
